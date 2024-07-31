856593e24e632e574f873c3831d2cbb0affd49ef
Amir javeed <amir@vaultspay.ae>
2024-07-31 11:38:16 +0400

    pull to refresh with offline data and pagination implemented

--- paginationjetpackcompose/src/main/java/com/example/paginationjetpackcompose/ui/screens/ItemListScreen.kt
+++ paginationjetpackcompose/src/main/java/com/example/paginationjetpackcompose/ui/screens/ItemListScreen.kt
@@ -1,13 +1,24 @@
 package com.example.paginationjetpackcompose.ui.screens
 
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.pulltorefresh.PullToRefreshContainer
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
 import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
@@ -15,15 +26,39 @@ import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.items
 import com.example.paginationjetpackcompose.models.Item
 import com.example.paginationjetpackcompose.repository.ItemRepository
+import kotlinx.coroutines.delay
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ItemList1(
     modifier: Modifier = Modifier,
     viewModel: ItemListViewModel = ItemListViewModel(repository = ItemRepository()),
 ) {
     val items1 = viewModel.items.collectAsLazyPagingItems()
+    val pullToRefreshState = rememberPullToRefreshState()
+
+
+    LaunchedEffect(pullToRefreshState.isRefreshing) {
+        if (pullToRefreshState.isRefreshing) {
+            delay(1000)
+            pullToRefreshState.endRefresh()
+
+        }
+    }
+
+    Box(
+        modifier = modifier
+            .nestedScroll(pullToRefreshState.nestedScrollConnection)
+    ) {
+
     ListContent(modifier = modifier.fillMaxSize(), items1 = items1)
+        PullToRefreshContainer(
+            state = pullToRefreshState,
+            modifier = Modifier
+                .align(Alignment.TopCenter),
+        )
+    }
 
 }
 
