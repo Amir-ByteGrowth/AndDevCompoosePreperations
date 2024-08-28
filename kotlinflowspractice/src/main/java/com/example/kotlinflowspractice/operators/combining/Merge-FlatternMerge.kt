883c2fec403fe07f9f6d6bd24c0111239bb7b4a3
Amir javeed <amir@vaultspay.ae>
2024-08-28 15:28:44 +0400

    merge and flat merge added

--- kotlinflowspractice/src/main/java/com/example/kotlinflowspractice/operators/combining/Merge-FlatternMerge.kt
+++ kotlinflowspractice/src/main/java/com/example/kotlinflowspractice/operators/combining/Merge-FlatternMerge.kt
@@ -1,7 +1,10 @@
 package com.example.kotlinflowspractice.operators.combining
 
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.flattenMerge
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.runBlocking
 
 val sourceFlowA = flow {
@@ -16,6 +19,18 @@ val sourceFlowB = flow {
     emit("Flow B - Emission 2")
 }
 
-fun main() = runBlocking {
+// flatten merge is used when we have flow of flows
+val flowOfFlows = flowOf(sourceFlowA, sourceFlowB)
 
+fun main() = runBlocking {
+    // will give emits from both flows
+//    merge(sourceFlowA, sourceFlowB).collectLatest {
+//        println(it)
+//    }
+    // concourrancy will manage in flight flows means if its value is 1 it will show all emits from
+    // flow 1 and then from flow 2. if its values is two it will show concurrency accordingly
+//    flowOfFlows.flattenMerge(1).collectLatest { println(it) }
+
+// it is used only when we have flow of flows
+    flowOfFlows.flattenMerge().collectLatest { println(it) }
 }
