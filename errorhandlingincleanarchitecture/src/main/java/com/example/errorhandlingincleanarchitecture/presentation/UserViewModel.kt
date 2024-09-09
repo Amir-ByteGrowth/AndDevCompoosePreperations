467d7213eb6d101e49b05f40eebc209439ae6e5d
Amir javeed <amir@vaultspay.ae>
2024-09-09 16:45:13 +0400

    Error Handling And Error Code Parsing Done

--- errorhandlingincleanarchitecture/src/main/java/com/example/errorhandlingincleanarchitecture/presentation/UserViewModel.kt
+++ errorhandlingincleanarchitecture/src/main/java/com/example/errorhandlingincleanarchitecture/presentation/UserViewModel.kt
@@ -3,9 +3,10 @@ package com.example.errorhandlingincleanarchitecture.presentation
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.errorhandlingincleanarchitecture.domain.AuthRepository
-import com.example.errorhandlingincleanarchitecture.domain.DataError
 import com.example.errorhandlingincleanarchitecture.domain.Result
 import com.example.errorhandlingincleanarchitecture.domain.UserDataValidator
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 
 // we can assume user data validator here as use case
@@ -14,6 +15,10 @@ class UserViewModel(
     private val userDataValidator: UserDataValidator,
     private val repository: AuthRepository,
 ) : ViewModel() {
+
+    private val eventChannel = Channel<UserEvent>()
+    val events=eventChannel.receiveAsFlow()
+
 
     fun onRegisterClick(password: String) {
         when (val result = userDataValidator.validatePassword(password)) {
@@ -35,15 +40,20 @@ class UserViewModel(
 
             when (val resut = repository.register(password)) {
                 is Result.Error -> {
-                    when (resut.error) {
-                        DataError.Network.REQUEST_TIMEOUT -> TODO()
-                        DataError.Network.TOO_MANY_REQUESTS -> TODO()
-                        DataError.Network.NO_INTERNET -> TODO()
-                        DataError.Network.PAYLOAD_TOO_LARGE -> TODO()
-                        DataError.Network.SERVER_ERROR -> TODO()
-                        DataError.Network.SERIALIZATION -> TODO()
-                        DataError.Network.UNKNOWN -> TODO()
-                    }
+//                    when (resut.error) {
+//                        DataError.Network.REQUEST_TIMEOUT -> TODO()
+//                        DataError.Network.TOO_MANY_REQUESTS -> TODO()
+//                        DataError.Network.NO_INTERNET -> TODO()
+//                        DataError.Network.PAYLOAD_TOO_LARGE -> TODO()
+//                        DataError.Network.SERVER_ERROR -> TODO()
+//                        DataError.Network.SERIALIZATION -> TODO()
+//                        DataError.Network.UNKNOWN -> TODO()
+
+//                    }
+
+                    //      we have implemented error parsing in asUiTextFile wo we will will do this as
+                    val errorMessage = resut.error.asUiText()
+                    eventChannel.send(UserEvent.Error(errorMessage))
                 }
 
                 is Result.Success -> {
@@ -55,4 +65,8 @@ class UserViewModel(
     }
 
 
+}
+
+sealed interface UserEvent {
+    data class Error(val error: UiText) : UserEvent
 }
